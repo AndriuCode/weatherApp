5e5c52cd4a94a77e0d6692219afcd598b4d45ef7
AndriuCode <vivanco.sebastian.a@gmail.com>
2023-10-24 12:51:47 -0500

    Clean up the project 🔥

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Providers } from './providers'
-import { Inter } from 'next/font/google'
 
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
     title: 'Create Next App',
@@ -18,7 +15,7 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en">
-            <body className={inter.className}>
+            <body>
                 <Providers>
                     {children}
                 </Providers>
